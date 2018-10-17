ffea3ff8815f51a55a1dddf5e24d76f7116b9752
chendersUofO <chenders@uoregon.edu>
2018-10-16 20:19:12 -0700

    Adding files

---
+++ Chilled-Beer-puppet/manifests/site.pp
@@ -0,0 +1,28 @@
+node ip-10-0-1-25 {
+
+	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/3",
+	}
+	include sshd
+    }
+
+    node ip-10-0-1-24 {
+	include sshd
+	#include apache   
+	 }
+
+    node ip-10-0-3-101 {
+	include sshd
+	include apache
+	include useraccounts
+	include wordpress	
+}
+
+    #node ip-10-0-3-199{
+	#include sshd
+	#}
+
+    #node ip-10-0-1-125 {
+    #}
---
+++ Chilled-Beer-puppet/modules/apache/manifests/init.pp
@@ -0,0 +1,46 @@
+#class { 'apache': }
+# Files attempting to submit for apache week 4 project
+
+
+class apache {
+    package {
+      "apache2": ensure => installed;
+    }
+
+file {"/etc/apache2/apache2.conf":
+  source =>  [
+    "puppet:///modules/apache/apache2.conf",
+  ],
+
+  mode => 444,
+  owner => root,
+  group => root,
+
+  require => Package["apache2"],
+	}
+
+file {"/var/www/html":
+  source => [
+    "puppet:///modules/apache/html"
+  ],
+  recurse => true,
+  ensure => directory,
+  mode => 755,
+  owner => root,
+  group => root,
+
+  #require => Package["apache"],
+}
+
+service { "apache2":
+  enable => true,
+  ensure => running,
+  hasstatus => true,
+  hasrestart => true,
+
+  require => [ Package["apache2"], 
+              File["/etc/apache2/apache2.conf"] ],
+
+  subscribe => File["/etc/apache2/apache2.conf"]
+  }
+}
